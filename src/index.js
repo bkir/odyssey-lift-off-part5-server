5c38fdeb95ae0f6969fafd298e404eaa97d4b80c
Ben Kirberger <benkirberger@gmail.com>
2022-03-30 08:46:10 -0500

    how do i fix this

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 const TrackAPI = require("./datasources/track-api");
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-console.log(
-  `process.env.NODE_TLS_REJECT_UNAUTHORIZED = ${process.env.NODE_TLS_REJECT_UNAUTHORIZED}`
-);
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+// console.log(
+//   `process.env.NODE_TLS_REJECT_UNAUTHORIZED = ${process.env.NODE_TLS_REJECT_UNAUTHORIZED}`
+// );
 
 async function startApolloServer(typeDefs, resolvers) {
   const server = new ApolloServer({
